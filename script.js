f916d3030569ee303a206650af144ac4098b9a97
Romain <romainyvernes@gmail.com>
2021-03-17 20:54:06 -0700

    Completed add form, basic book cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function Book(title, author, year, pageCount, read) {
+function Book(title, author, pageCount, year, read) {
     this.title = title;
     this.author = author;
     this.pageCount = pageCount;
@@ -12,8 +12,28 @@ function Label(name, content, value) {
     this.value = value;
 }
 
-function addBookToLibrary(book) {
-    library.push(book);
+function addBookToLibrary(event) {
+    /* cancel add if one of the fields is empty and change display of missing 
+    fields */
+    let abort = false;
+
+    abort = enableError(abort);
+    
+    if (abort) {
+        // prevent form from submitting and closing itself
+        return event.preventDefault();
+    }
+    
+    let newBook = new Book(event.target[1].value, event.target[2].value, event.target[3].value, 
+        event.target[4].value, (event.target[5].value === 'true' ? true : false));
+    
+    library.push(newBook);
+
+    // prevent page refresh, which resets value of library to empty array
+    event.preventDefault();
+
+    hideForm();
+    viewBooks();
 }
 
 function createBookCard(book) {
@@ -21,7 +41,7 @@ function createBookCard(book) {
     bookCard.className = 'book';
 
     const CARD_LABELS = [new Label('title', null, book.title), new Label('author',
-            'Author:', book.author), new Label('date', 'Publication Year:', 
+            'Author:', book.author), new Label('date', 'Year of Publication:', 
             book.year), new Label('pages', 'Number of Pages:', book.pageCount)];
 
     CARD_LABELS.map(cardLabel => {
@@ -30,12 +50,37 @@ function createBookCard(book) {
         cardItem.textContent = `${cardLabel.content ? cardLabel.content : ''} ${cardLabel.value}`;
         bookCard.appendChild(cardItem);
     });
+    
+    // create container with mark as read message and toggle button
+    const readBtnContainer = document.createElement('div');
+    const readMsg = document.createElement('p');
+    
+    readBtnContainer.className = 'read-container';
+    readMsg.textContent = 'Mark as read';
+    readBtnContainer.appendChild(readMsg);
+
+    // create toggle button
+    const toggleContainer = document.createElement('label');
+    const toggleInput = document.createElement('input');
+    const toggleSpan = document.createElement('span');
+    
+    toggleContainer.className = 'toggle-container';
+    toggleInput.className = 'toggle-input';
+    toggleInput.type = 'checkbox';
+    toggleSpan.className = 'toggle-span';
+    
+    toggleContainer.appendChild(toggleInput);
+    toggleContainer.appendChild(toggleSpan);
+    readBtnContainer.appendChild(toggleContainer);
+
+    bookCard.appendChild(readBtnContainer);
 
     return bookCard;
 }
 
 function viewBooks() {
     const displayContainer = document.querySelector('#book-display');
+    displayContainer.textContent = '';
     library.map(book => {
         const bookCard = createBookCard(book);
         displayContainer.appendChild(bookCard);
@@ -43,17 +88,59 @@ function viewBooks() {
 }
 
 function displayForm() {
-    console.log("success")
+    form.reset();
+    disableError();
+    formContainer.style.display = 'block';
+}
+
+function hideForm() {
+    formContainer.style.display = 'none';
+}
+
+function enableError(abort) {
+    const fields = form.querySelectorAll('input, select');
+    fields.forEach(field => {
+        field.style.borderColor = 'rgb(151, 151, 151)';
+        const errorMsg = field.previousElementSibling;
+        errorMsg.style.display = 'none';
+        if (field.value === '') {
+            abort = true;
+            field.style.borderColor = 'red';         
+            errorMsg.style.display = 'inline-block';
+        }
+    })
+    return abort;
+}
+
+function disableError() {
+    const fields = form.querySelectorAll('input, select');
+    fields.forEach(field => {
+        field.style.borderColor = 'rgb(151, 151, 151)';
+        const errorMsg = field.previousElementSibling;
+        errorMsg.style.display = 'none';
+    });
 }
 
 let library = [];
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '1937', '295', false);
-const theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', '1954', '1137', false);
-addBookToLibrary(theHobbit);
-addBookToLibrary(theLordOfTheRings);
+const formContainer = document.querySelector('#form-container');
+formContainer.addEventListener('click', e => {
+    if (e.target.id === 'form-container') {
+        hideForm();
+    }
+});
 
-const newBookBtn = document.querySelector('#add-book');
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', '1937', false);
+const theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', '1137', '1954', false);
+library.push(theHobbit, theLordOfTheRings);
+
+const form = document.querySelector('#add-form');
+form.addEventListener('submit', addBookToLibrary);
+
+const newBookBtn = document.querySelector('#add-btn');
 newBookBtn.addEventListener('click', displayForm);
+
+const closeFormBtn = document.querySelector('#close-form-btn');
+closeFormBtn.addEventListener('click', hideForm);
 
 viewBooks();
